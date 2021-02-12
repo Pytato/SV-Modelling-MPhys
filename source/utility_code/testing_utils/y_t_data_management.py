27501c8c3bef533bb3b68ef1946955b42f06fc54
Pytato <Pytato@users.noreply.github.com>
2021-02-12 01:08:26 +0000

    Signature updates for consistency

--- source/utility_code/testing_utils/y_t_data_management.py
+++ source/utility_code/testing_utils/y_t_data_management.py
@@ -4,12 +4,12 @@ import os
 import numpy as np
 
 
-def generate_test_y_t_data(eta_var_l, mu_l, phi_l):
+def generate_test_y_t_data(phi_l, mu_l, eta_var_l):
     DATA_FILE_LOC = "./y_t_model_data_output.npy"
     if os.path.exists(DATA_FILE_LOC):
         temp = np.load(DATA_FILE_LOC)
         return temp
 
-    model_output_data = gen_y_t_vals(eta_var_l, mu_l, phi_l, num_steps_to_gen=300000)
+    model_output_data = gen_y_t_vals(phi_l, mu_l, eta_var_l, num_steps_to_gen=300000)
     np.save(DATA_FILE_LOC, model_output_data)
     return model_output_data
